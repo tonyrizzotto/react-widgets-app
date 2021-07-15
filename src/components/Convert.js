ff5da647cce14e485a5f046528f737d72a68bbe6
Tony Rizzotto <anthonyrizzotto@gmail.com>
2021-07-15 09:51:22 -0400

    Update: Debounced Term
    Application how has 2 pieces of state, one of which is set on a timer. This makes it so there are only requests made after a user finishes typing (approx. 500ms after the last keystroke.

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -3,6 +3,27 @@ import axios from 'axios';
 
 const Convert = ({ language, text }) => {
   const [translated, setTranslated] = useState('');
+
+  const [debouncedText, setDebouncedText] = useState(text);
+
+  //need to create 2 useEffects to implement a debounced term. One as a timer that auto refreshes and another that actually makes the request.
+
+  //useEffect #1 - updated the debouncedTerm state and cleanup every 500ms. Should run when initally reloaded, or when text is updated.
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      //update debounced text
+      setDebouncedText(text);
+    }, 500);
+
+    //clear out the timerId
+    return () => {
+      clearTimeout(timerId);
+      {
+      }
+    };
+  }, [text]);
+
+  //useEffect #2 - make a request using the debounced term piece of state
   useEffect(() => {
     const doTranslation = async () => {
       const { data } = await axios.post(
@@ -10,7 +31,7 @@ const Convert = ({ language, text }) => {
         {},
         {
           params: {
-            q: text,
+            q: debouncedText,
             target: language.value,
             key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
           },
@@ -20,7 +41,7 @@ const Convert = ({ language, text }) => {
     };
 
     doTranslation();
-  }, [language, text]);
+  }, [language, debouncedText]);
   return (
     <div>
       <h2>{translated}</h2>
